511c7763446ac33f8905a736bb6b8a8760cb171f
masha23x <132945855+masha23x@users.noreply.github.com>
2023-05-08 22:57:08 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector('.hamburger');
+const close = document.querySelector('.close');
+const overlay = document.querySelector('.overlay');
+const mobileMenu = document.querySelector('.mobile');
+const header = document.querySelector('header')
+
+hamburger.addEventListener('click', () => {
+    mobileMenu.classList.add('mobile_active');
+    overlay.classList.add('overlay_active');
+    hamburger.style.opacity = '0';
+})
+
+close.addEventListener('click', () => {
+    mobileMenu.classList.remove('mobile_active');
+    overlay.classList.remove('overlay_active');
+    hamburger.style.opacity = '1';
+})
+
